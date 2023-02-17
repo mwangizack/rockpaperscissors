ae9e49831eff3e3e2743d28d5e301941ed98a57a
mwangizack <ndirangu.zmwangi@gmail.com>
2023-02-17 11:52:55 +0300

    Cache the DOM in main.js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// caching the DOM - storing reference points for future use
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
